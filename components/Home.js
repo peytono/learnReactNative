f07b22ba9a71dbadc15d8cf994f50175d3125ab5
peytono <pcopcoeco@gmail.com>
2024-02-25 13:35:19 -0600

    reactnative cat photo on home

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,15 @@
-import { View, Text, Button, Alert } from 'react-native';
-import { useState } from 'react';
+import { View, Text, Button, Alert, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+// import { useState } from 'react';
 
 export default function Home(){
   
   return (
     <View>
-      <Text>Here is the Home View</Text>
+      <Text style={{fontSize: 20}}>Here is the Home View</Text>
       <Button title='Do Something' onPress={() => Alert.alert('You did something!')}/>
+      <StatusBar />
+      <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }} style={{ width: 200, height: 200 }} />
     </View>
   );
 }
